d81e14e9ce0a6eff0034eca2c0e76a09c5d4e564
Jany Demcak <jademcak@gmail.com>
2016-11-02 12:42:25 +0100

    feat(syna.c)

--- inter/ifj-syna.c
+++ inter/ifj-syna.c
@@ -6,6 +6,7 @@
 
  #include "ifj-syna.h"
  #include "ifj-inter.h"
+ #include <stdbool.h>
 
  //rules
  //terminals
@@ -15,6 +16,22 @@
   * Run syntactic analysis
   * @returns 0 when successful
   */
+  bool start(ifjInter *self)
+  {
+    bool return_value = false;
+    token * active;
+    token * next;
+    if ((active = lexa_next_token(self->lexa_module,self->table) ==  T_CLASS) && (lexa_next_token(self->lexa_module,self->table) == T_IDENTIFIER))
+    {
+      return_value = class_inside() && next();
+      symbol_table * class_table = ial_symbol_table_new();
+      class_table->parent = self->table;
+      token->child = class_table;
+    }
+  }
+  bool class_inside(ifjInter *self, token )
+
+
  int syna_run( ifjInter *self)
  {
      token * active;
@@ -45,4 +62,5 @@
      while(active && active->type != T_END);
 
      return 0;
+     // janny skusa pridat nieco noveho
  }
